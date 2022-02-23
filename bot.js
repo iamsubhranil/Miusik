e9e137b26d90b96c0011aa4cba18a9eb31380d42
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-02-23 18:35:48 +0530

    updt: bind to a port when running on heroku

--- bot.js
+++ bot.js
@@ -1,5 +1,20 @@
 require("dotenv").config();
 
+// bind to a port if we're running on heroku, otherwise we're gonna
+// get killed
+if(process.env.HEROKU) {
+    const http = require('http');
+
+    const requestListener = function (req, res) {
+    res.writeHead(200);
+        res.end('Hi there! Glad you visited the website, but unfortunately it does not have a frontend right now!');
+    }
+
+    const server = http.createServer(requestListener);
+    server.listen(8080);
+
+}
+
 const { COMMANDS } = require("./deploy_commands.js");
 
 const {
